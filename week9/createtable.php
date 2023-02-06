302afc129275fc3a5ffe0642f7e134e72c6ec0c7
rterania <rterania@apc.edu.ph>
2023-02-06 20:36:48 +0800

    sql insert

--- week9/createtable.php
+++ week9/createtable.php
@@ -5,7 +5,7 @@ $password = "";
 $dbname = "commission";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new commission ($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
