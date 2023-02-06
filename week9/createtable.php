e17e6d52a5ff177d9946c57ca4a08aefe5560b9c
rterania <rterania@apc.edu.ph>
2023-02-06 19:07:47 +0800

    sql insert

--- week9/createtable.php
+++ week9/createtable.php
@@ -13,12 +13,11 @@ if ($conn->connect_error) {
 
 // sql to create table
 $sql = "CREATE TABLE request (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
+firstname VARCHAR(30) PRIMARY KEY,
 email VARCHAR(30) NOT NULL,
-payment VARCHAR(50),
-comment VARCHAR(50),
-style VARCHAR(50),
+payment VARCHAR(50) NOT NULL,
+comment VARCHAR(50) NOT NULL,
+style VARCHAR(50) NOT NULL,
 reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 )";
 
