119271210dd8e11eb100a68f2c3c013fc8c8299e
rterania <rterania@apc.edu.ph>
2023-02-06 17:04:03 +0800

    sql insert

--- week9/complete_validation.php
+++ week9/complete_validation.php
@@ -63,6 +63,43 @@ echo $message;
 echo "<br>";
 echo $style;
 ?>
+
+
+
+
+<?php
+if ($_SERVER ["REQUEST_METHOD"] == "POST") {
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO MyGuests (firstname, payment, email)
+VALUES ('$name', '$payment', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+}
+?>
+
+
+
+
+
  </div>
 </body>
 </html>
