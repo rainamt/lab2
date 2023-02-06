aa26124baa2a64e67f608e33274fab8482a36fd6
rterania <rterania@apc.edu.ph>
2023-02-06 19:25:36 +0800

    sql insert

--- week9/createtable.php
+++ week9/createtable.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "commissions";
+$dbname = "mydb";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
