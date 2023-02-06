3dd607d5c9a9ec922114b1c0e3382726f3fce03e
rterania <rterania@apc.edu.ph>
2023-02-06 18:15:46 +0800

    sql insert

--- week9/complete_validation.php
+++ week9/complete_validation.php
@@ -76,7 +76,7 @@ $password = "";
 $dbname = "requests";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new requests($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
