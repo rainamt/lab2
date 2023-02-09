2c8e9f55731c0d16c2a49c8fc973eff6fa5f7b8c
rterania <rterania@apc.edu.ph>
2023-02-09 20:28:23 +0800

    mi212

--- week9/sqlconnect.php
+++ week9/sqlconnect.php
@@ -1,5 +1,3 @@
-ines (10 sloc)  262 Bytes
-
 <?php
 $servername = "localhost";
 $username = "webprogmi212";
@@ -10,5 +8,13 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+
+$sql = "CREATE DATABASE DBmi212";
+if ($conn->query($sql) === TRUE) {
+  echo "Database created successfully";
+} else {
+  echo "Error creating database: " . $conn->error;
+}
+
+$conn->close();
 ?>
